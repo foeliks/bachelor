6a3d250d85d54b48bb7821890408474def98045f
Felix Sager <felix.sager@alumni.fh-aachen.de>
2020-10-07 13:41:31 +0200

    First Steps

---
+++ database/init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE TABLE test (
+	    user_id serial PRIMARY KEY,
+	    username VARCHAR ( 50 ) UNIQUE NOT NULL,
+	    password VARCHAR ( 50 ) NOT NULL,
+	    email VARCHAR ( 255 ) UNIQUE NOT NULL,
+	    created_on TIMESTAMP NOT NULL,
+            last_login TIMESTAMP 
+    ;
+EOSQL
