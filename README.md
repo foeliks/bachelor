f77304091c76c5f3ff12634422c5139734bda3a5
fs8780s <felix.sager@alumni.fh-aachen.de>
2020-10-07 18:57:29 +0200

    Vorbereitung DB fertig?

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Hier entsteht ein Full-Stack Projekt für eine spielerische JavaScript Lernplattform
+
+Alle Docker Container stoppen
+    sudo docker stop $(docker ps -q)
+
+Docker Daemon starten
+    sudo systemctl start docker
--- database/init.sh
+++ database/init.sh
@@ -2,12 +2,8 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE TABLE test (
-	    user_id serial PRIMARY KEY,
-	    username VARCHAR ( 50 ) UNIQUE NOT NULL,
-	    password VARCHAR ( 50 ) NOT NULL,
-	    email VARCHAR ( 255 ) UNIQUE NOT NULL,
-	    created_on TIMESTAMP NOT NULL,
-            last_login TIMESTAMP 
-    ;
+    CREATE TABLE IF NOT EXISTS test2 (
+	    user_id serial PRIMARY KEY
+    );
+    INSERT INTO test2 VALUES (1), (2);
 EOSQL
