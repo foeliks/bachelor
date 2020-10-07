1270acfaa5905d562930fd238d45bd81ebb79bc4
fs8780s <felix.sager@alumni.fh-aachen.de>
2020-10-07 18:58:28 +0200

    Erster Versuch

---
+++ develop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "######### Installer für Robob Umgebung #########\n"
+
+echo "0. apt Update"
+apt-get update
+
+echo "1. Installation Postresql (Für Die Datenbank):"
+sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+apt-get -y install postgresql
+
+echo "2. Installation Nodejs/Reactjs"
+apt install npm
+npm install -g create-react-app
+
+echo "3. Installation Docker"
+apt install docker docker-compose 
+systemctl start docker
+docker-compose up -d
