240cd7c9725bcf0b7ed6a577054d94c99045744a
Felix <felix.sager@alumni.fh-aachen.de>
2020-10-21 15:16:13 +0200

    wsgi flasche version?

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css'
 import './index.css';
 import Login from './auth/Login.js';
+import Categories from './categories/Categories.js';
 
 ReactDOM.render(
   <React.StrictMode>
     <Login />
+    <Categories />
   </React.StrictMode>,
   document.getElementById('root')
 );
