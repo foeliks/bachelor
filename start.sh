6ad6d7faf720a29564f57b8825abee8582272a34
Felix <felix.sager@alumni.fh-aachen.de>
2020-11-23 15:46:47 +0100

    Unity eingebunden

--- start.sh
+++ start.sh
@@ -2,5 +2,9 @@ apt install docker docker-compose
 systemctl start docker
 
 chmod -R a+rw ./database/import
+chmod -R a+rwx ./frontend/game
+
+cp -R ./game/Build ./frontend/public
+cp -R ./game/TemplateData ./frontend/TemplateData
 
 docker-compose up --build -d
