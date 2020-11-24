85a236dd6652153832a64143b04f3bd00b3e43cc
Felix Sager <felix.sager@alumni.fh-aachen.de>
2020-11-24 18:46:33 +0100

    start script fix

--- start.sh
+++ start.sh
@@ -2,9 +2,10 @@ apt install docker docker-compose
 systemctl start docker
 
 chmod -R a+rw ./database/import
-chmod -R a+rwx ./game
 
 cp -R ./game/Build ./frontend/public
 cp -R ./game/TemplateData ./frontend/public
+
+chmod -R a+rwx ./frontend/public
 
 docker-compose up --build -d
