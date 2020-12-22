0f3f7a23b781c7e75bde27c71a0beba635a1587b
Felix <felix.sager@alumni.fh-aachen.de>
2020-12-22 22:26:44 +0100

    kleine Fixes

---
+++ frontend/build/precache-manifest.151b29e661956c50ed805835a4f11bfe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "515089f62c9c14e3aabe316e392a236f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd8f0197b3c63aa671b5",
+    "url": "/static/css/2.ef440c72.chunk.css"
+  },
+  {
+    "revision": "5f21bd1ca91d5e5fb416",
+    "url": "/static/css/main.a140ab9d.chunk.css"
+  },
+  {
+    "revision": "fd8f0197b3c63aa671b5",
+    "url": "/static/js/2.5fe2d8d8.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.5fe2d8d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f21bd1ca91d5e5fb416",
+    "url": "/static/js/main.6b942e13.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "d9ab192896af1ab48c760f3368842b79",
+    "url": "/static/media/Hack-Regular.d9ab1928.ttf"
+  }
+]);
