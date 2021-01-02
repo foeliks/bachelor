cf7cb0d2d26650bacf3f74fecb76a021d1ffe496
fs8780s <felix.sager@alumni.fh-aachen.de>
2021-01-02 20:13:40 +0100

    bugfix: richtige spielversion

--- start.sh
+++ start.sh
@@ -6,6 +6,9 @@ chmod -R a+rw ./database/import
 cp -R ./game/Build ./frontend/public
 cp -R ./game/TemplateData ./frontend/public
 
+cp -R ./game/Build ./frontend/build
+cp -R ./game/TemplateData ./frontend/build
+
 chmod -R a+rwx ./frontend/public
 chmod -R a+rwx ./frontend/build
 
